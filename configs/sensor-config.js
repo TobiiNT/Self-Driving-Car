95eb6a4ea3804c07c72828f510984bccc0163614
Nhật Trường <nhattruongtotran@gmail.com>
2023-03-20 14:25:24 +0700

    Expand spread ratio of sensor

--- configs/sensor-config.js
+++ configs/sensor-config.js
@@ -1,5 +1,5 @@
 class SensorConfig {
-    constructor(count = 5, length = 150, speadRatio = 3) {
+    constructor(count = 5, length = 150, speadRatio = 2) {
         this.rayCount = count;
         this.rayLength = length;
         this.raySpread = Math.PI / speadRatio;
